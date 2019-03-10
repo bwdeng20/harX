fcb14c20cae15598e359f4b93ffec1e7a3488deb
Bowen9s <dbwtimteo@outlook.com>
2019-03-10 09:53:20 +0800

    basic predict function added

--- integration_test/EvaluationTest.py
+++ integration_test/EvaluationTest.py
@@ -1,22 +1,39 @@
 from utils.datasets import UCIHAR
-from utils.preprocess import Si2Ai
+from utils.preprocess import Si2Ai, ucihar4convlstm
 from torch.utils.data import DataLoader
-from model.MoDeX import JY15CNN3
+from model.MoDeX import JY15CNN3, ConvLstm
 from trainer.Coach import Coach
 
-
 if __name__ == "__main__":
-    test_dir=r'D:\DataSets\Activity_Recognition\SensorBased\UCI HAR Dataset\UCI HAR Dataset\test'
-    testset = UCIHAR(dir=test_dir,transform=Si2Ai())
-    test_loader = DataLoader(testset,421 ,True, num_workers=2)
+    test_dir = r'D:\DataSets\Activity_Recognition\SensorBased\UCI HAR Dataset\UCI HAR Dataset\test'
+    testset = UCIHAR(dir=test_dir, transform=Si2Ai())
+    test_loader = DataLoader(testset, 421, True, num_workers=2)
 
     net1 = JY15CNN3(6)
-    net1.load_weights(r"D:\WalkingTrajectoryEstimation\Projects\checkpoints\LeakyOne\JY15CNN3")
-    coach = Coach(net1, cpdir=r"../checkpoints/LeakyOne/", logdir=r'../logs/LeakyJY15CNN/')
+    net1.load_weights(r"D:\WalkingTrajectoryEstimation\harX\checkpoints\LeakyOne\JY15CNN3")
+    coach = Coach(net1)
     coach.evaluate(test_loader)
 
-    c_mat=coach.evaluations['confusionMatrix']
+    c_mat = coach.evaluations['confusionMatrix']
     print(c_mat)
     from utils.ProcessedVisual import visual_cm
-    visual_cm(coach.evaluations['confusionMatrix'],classes=testset.activity.values())
+
+    visual_cm(coach.evaluations['confusionMatrix'], classes=testset.activity.values())
     print(c_mat.sum())
+# ----------------------------------------------------------------------------------------------------
+    test_dir = r'D:\DataSets\Activity_Recognition\SensorBased\UCI HAR Dataset\UCI HAR Dataset\test'
+    testset = UCIHAR(dir=test_dir, transform=ucihar4convlstm())
+    testloader = DataLoader(testset, batch_size=30)
+    net2 = ConvLstm(6, 9)
+    net2.load_weights(
+        r"D:\WalkingTrajectoryEstimation\harX\checkpoints\ConvLstm1\ConvLstm-trained")
+    coach=Coach(net2)
+    coach.evaluate(testloader)
+    c_mat = coach.evaluations['confusionMatrix']
+    print(c_mat)
+    from utils.ProcessedVisual import visual_cm
+
+    visual_cm(coach.evaluations['confusionMatrix'], classes=testset.activity.values())
+    print(c_mat.sum())
+
+
